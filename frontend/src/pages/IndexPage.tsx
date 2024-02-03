1fe2c0743c673792c6a1840140a7202191f3e5f8
Mix <32300164+mnixry@users.noreply.github.com>
2024-02-03 18:55:00 +0800

    Add About modal with GitHub link

--- frontend/src/pages/IndexPage.tsx
+++ frontend/src/pages/IndexPage.tsx
@@ -1,18 +1,29 @@
 import {
   AppShell,
+  Button,
   Center,
+  Divider,
   Grid,
   Group,
+  Modal,
   SegmentedControl,
   Text,
   rem,
 } from "@mantine/core";
-import { IconBoxSeam, IconBinary, IconBinaryOff } from "@tabler/icons-react";
+import {
+  IconBoxSeam,
+  IconBinary,
+  IconBinaryOff,
+  IconBrandGithub,
+} from "@tabler/icons-react";
 import AssemblerPane from "../components/AssemblerPane";
 import { useState } from "react";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function IndexPage() {
   const [mode, setMode] = useState<string>("asm");
+  const [aboutOpened, { open: openAbout, close: closeAbout }] =
+    useDisclosure(false);
 
   return (
     <>
@@ -61,7 +72,52 @@ export default function IndexPage() {
                 />
               </Grid.Col>
               <Grid.Col span="content">
-                <Text>About</Text>
+                <Button onClick={openAbout} variant="transparent">
+                  About
+                </Button>
+                <Modal
+                  opened={aboutOpened}
+                  onClose={closeAbout}
+                  title="About"
+                  overlayProps={{
+                    backgroundOpacity: 0.55,
+                    blur: 3,
+                  }}
+                >
+                  <Text size="sm">
+                    This is a web-based assembler and disassembler, utilizing
+                    GNU Binutils and ported to WebAssembly, thus functioning as
+                    a purely static website. The development of this website is
+                    facilitated by the use of Mantine and Vite.
+                  </Text>
+                  <Button
+                    leftSection={<IconBrandGithub />}
+                    fullWidth
+                    variant="outline"
+                    mt="md"
+                    onClick={() =>
+                      window.open("https://github.com/mnixry/binutils-wasm")
+                    }
+                  >
+                    Star on GitHub
+                  </Button>
+                  <Divider />
+                  <Text size="xs" pt="md" style={{ fontFamily: "monospace" }}>
+                    Copyright (C) {new Date().getFullYear()} Mix
+                    <Divider my="sm" />
+                    This program is free software: you can redistribute it
+                    and/or modify it under the terms of the GNU General Public
+                    License as published by the Free Software Foundation, either
+                    version 3 of the License, or (at your option) any later
+                    version. This program is distributed in the hope that it
+                    will be useful, but WITHOUT ANY WARRANTY; without even the
+                    implied warranty of MERCHANTABILITY or FITNESS FOR A
+                    PARTICULAR PURPOSE. See the GNU General Public License for
+                    more details. You should have received a copy of the GNU
+                    General Public License along with this program. If not, see
+                    https://www.gnu.org/licenses/.
+                  </Text>
+                </Modal>
               </Grid.Col>
             </Grid>
           </Center>
