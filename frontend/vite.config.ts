512b2976f0b20ab33f4f4af44652daccf68e2617
Mix <32300164+mnixry@users.noreply.github.com>
2025-06-25 03:13:22 +0800

    chore(config): integrate react-compiler and rolldown-vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
+import reactCompiler from "babel-plugin-react-compiler";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ babel: { plugins: [reactCompiler] } })],
   optimizeDeps: {
     exclude: ["@binutils-wasm/binutils", "@binutils-wasm/gas"],
   },
